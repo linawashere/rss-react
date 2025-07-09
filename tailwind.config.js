fa9318327bcd8365a1ae8254dac325f949d9271f
Polina <alllrrw@gmail.com>
2025-07-09 17:26:45 +0700

    feat: add dependencies, add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        text: ['var(--text-font)'],
+      }
+    },
+    fontSize: {
+      'xs': '1.2rem',
+      'sm': '1.4rem',
+      'base': '1.6rem',
+      'lg': '1.8rem',
+      'xl': '2rem',
+      '2xl': '2.4rem',
+      '3xl': '3rem',
+      '4xl': '3.6rem',
+      '5xl': '4.8rem',
+    },
+  },
+  plugins: [],
+};
