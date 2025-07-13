7e8bf53a7a75915caad12615c01756e190ecb7c9
Polina <alllrrw@gmail.com>
2025-07-13 19:24:31 +0700

    fix: fix error button

--- src/components/ErrorButton.tsx
+++ src/components/ErrorButton.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-class ErrorButton extends React.Component {
-  throwAnErorr = (): void => {
-    throw new Error('Error!');
+interface ErrorButtonState {
+  hasError: boolean;
+}
+
+class ErrorButton extends React.Component<
+  Record<string, never>,
+  ErrorButtonState
+> {
+  state = {
+    hasError: false,
+  };
+
+  throwError = () => {
+    this.setState({ hasError: true });
   };
 
   render() {
+    if (this.state.hasError) {
+      throw new Error('Test error from button!');
+    }
+
     return (
       <button
-        onClick={this.throwAnErorr}
+        onClick={this.throwError}
         className="w-32 m-auto p-2 bg-red-500 text-white rounded hover:bg-red-600"
       >
         Throw an Error
