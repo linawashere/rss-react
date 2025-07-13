471c792380f802f15308d6635581a8a4dc58f2b1
Polina <alllrrw@gmail.com>
2025-07-13 15:05:46 +0700

    feat: add components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+import React from 'react';
+import CardList from './components/CardList';
+import { searchAnime } from './api/jikan';
 
-function App() {
+class App extends React.Component {
+  state = {
+    animeList: [],
+    isLoading: false,
+    error: null,
+  };
 
-  return
+  componentDidMount() {
+    const savedQuery = localStorage.getItem('searchItem') || '';
+    this.fetchData(savedQuery);
+  }
+
+  fetchData = async (query = '') => {
+    try {
+      this.setState({ isLoading: true, error: null });
+      const data = await searchAnime(query);
+      this.setState({ animeList: data, isLoading: false });
+    } catch (err) {
+      this.setState({
+        error: `Failed to fetch data: ${err}`,
+        isLoading: false,
+      });
+    }
+  };
+
+  handleSearch = async (query: string) => {
+    this.fetchData(query);
+  };
+
+  render() {
+    return (
+      <div className="flex flex-col h-full">
+        <Header onSearch={this.handleSearch} />
+        <CardList
+          animeList={this.state.animeList}
+          isLoading={this.state.isLoading}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
