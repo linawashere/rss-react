44767fd1942d011e9b279e7ecc677032fa6c52f0
Polina <alllrrw@gmail.com>
2025-07-13 15:06:19 +0700

    refactor: header refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Search from './Search';
 import type HeaderProps from '../interfaces/headerProps';
 import React from 'react';
 class Header extends React.Component<HeaderProps> {
-    render() {
-        return (
-            <div className="header bg-gray-600">
-                <Search onSearch={this.props.onSearch}/>
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div className="header bg-gray-600">
+        <Search onSearch={this.props.onSearch} />
+      </div>
+    );
+  }
+}
 export default Header;
