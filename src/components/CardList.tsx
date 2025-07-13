7882d3af3a947939fcea03c9d4d88e3175558474
Polina <alllrrw@gmail.com>
2025-07-13 14:11:29 +0700

    feat: add cardList component

---
+++ src/components/CardList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from './Card';
+import type CardListProps from '../interfaces/card';
+
+class CardList extends React.Component<CardListProps> {
+  render() {
+    const { animeList, isLoading, error } = this.props;
+
+    if (error) {
+      return <div className="error-message text-red-500 p-4">{error}</div>;
+    }
+
+    if (isLoading) {
+      return <div className="loading-spinner">Loading...</div>;
+    }
+
+    if (animeList.length === 0) {
+      return <div>No items found</div>;
+    }
+
+    return (
+      <div className="card-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {animeList.map((anime, index) => (
+          <li key={index}>
+            <Card key={index} anime={anime} />
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
