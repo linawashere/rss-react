57b206d401164ee7f39b05080b4e70f67e29fce0
Polina <alllrrw@gmail.com>
2025-07-13 21:11:13 +0700

    fix: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/rss-react/',
+  build: {
+    assetsDir: 'assets',
+  },
   plugins: [react()],
   css: {
     postcss: './postcss.config.js'
